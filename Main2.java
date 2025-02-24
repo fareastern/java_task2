55b460c2f3dd70a43709ef95864d6811e998cdaa
fareastern <36926187+fareastern@users.noreply.github.com>
2025-02-24 14:58:25 +0300

    Add files via upload
    Задание 2 с выводом итоговой просчитанной суммы: доходы минус расходы

---
+++ Main2.java
@@ -0,0 +1,8 @@
+public class Main2 {
+    public static void main (String[] args){
+        int income = 100000;
+        int spending = 55000;
+        System.out.println("Итого (руб):");
+        System.out.println(income-spending);
+    }
+}
